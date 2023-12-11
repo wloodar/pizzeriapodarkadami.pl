5e23a8ae20a3e811e8c46bc45cdf17bc74d8c3b5
wloodar <wlodardev@gmail.com>
2023-12-11 16:07:12 +0100

    Removed previous analytics provider and used wlodev analytics instead

--- src/config/analytics.ts
+++ src/config/analytics.ts
@@ -1,11 +1,13 @@
 type AnalyticsConfig = {
     googleTagManagerId: false | string
     facebookPixelId: false | string
+    wlodevAnalyticsId: string
 }
 
 const analyticsConfig: AnalyticsConfig = {
     googleTagManagerId: false, // or "GTM-XXXXXX"
     facebookPixelId: false,
+    wlodevAnalyticsId: '8b018780-3742-4ad0-8e06-62b854fda9d5',
 }
 
 export { analyticsConfig }
